bf7b3b6d19308d7ef14716faefe18ba8fed0c88b
Clemwar <clemwar@gmail.com>
2020-01-06 20:27:02 +0100

    Users:Part2

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,15 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
@@ -22,13 +29,19 @@ class UserController extends AbstractController
     private $encoder;
 
     /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
      * UserController constructor.
      * @param UserRepository $repository
      */
-    public function __construct(UserRepository $repository, UserPasswordEncoderInterface $encoder)
+    public function __construct(UserRepository $repository, UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
     {
         $this->repository = $repository;
         $this->encoder = $encoder;
+        $this->em = $em;
     }
 
     /**
@@ -44,6 +57,179 @@ class UserController extends AbstractController
             'users' => $users,
             'current_menu' => 'admin',
             'admin_menu' => 'users'
+        ]);
+    }
+
+    /**
+     * @Route("/user/add", name="add_user")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function addUser(Request $request)
+    {
+        $user = new User();
+
+        // On crée le FormBuilder en appelant le formtype
+        $form = $this->createForm(UserType::class, $user);
+
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $user contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
+
+            // On vérifie que les valeurs entrées sont correctes
+            if ($form->isValid()) {
+
+                //On traite l'ajout d'image
+                /** @var UploadedFile $image */
+                $image = $form['photo']->getData();
+
+                // On vérifie la présence d'un fichier uploadé
+                if ($image) {
+                    $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $image->guessExtension();
+
+                    //On place le fichier dans le dossier prévu sur le serveur
+                    try {
+                        $image->move(
+                            $this->getParameter('photos_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        $this->addFlash('warning', 'L\'envoi de la photo a échoué');
+                    }
+
+                    // On ajoute le nom de l'image à l'utilisateur concerné
+                    $user->setPhoto($newFilename);
+                }
+
+                $this->addFlash('success', 'Bienvenue, inscription terminée');
+
+                // On enregistre notre objet $user dans la base de données, par exemple
+                $this->em->persist($user);
+                $this->em->flush();
+
+                // On redirige vers la page de connexion
+                return $this->redirectToRoute('login', [
+                    'current_menu' => 'login'
+                ]);
+            }
+        }
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+        return $this->render('/pages/form.html.twig', [
+            'form' => $form->createView(),
+            'current_menu' => 'login'
+        ]);
+    }
+
+    /**
+     * @Route("/pwup/user/delete/{id}", name="admin_delete_user", methods="DELETE")
+     */
+    public function deleteUser ($id, Request $request){
+
+        $user = $this->repository->find($id);
+
+        if ($this->isCsrfTokenValid('delete' . $id, $request->get('_token'))) {
+
+            //Je m'assure qu'une image est liée à l'utilisateur
+            if ($user->getPhoto() !== null) {
+                //J'appelle le gestionnaire de fichier
+                $filesystem = new Filesystem();
+                //Je vérifie si le fichier existe, si oui, symfony le supprime
+                if ($filesystem->exists($imageURL = $this->getParameter('photos_directory') . "/" . $user->getPhoto())) {
+                    $filesystem->remove([$imageURL]);
+                }
+            }
+
+            $this->em->remove($user);
+            $this->em->flush();
+        }
+
+        return $this->redirectToRoute('admin_users');
+    }
+
+    /**
+     * @Route("/user/update/{id}", name="update_user")
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function updateUser($id, Request $request)
+    {
+        $user = $this->repository->find($id);
+
+        // On crée le FormBuilder en appelant le formtype
+        $form = $this->createForm(UserType::class, $user);
+
+        // Si la requête est en POST
+        if ($request->isMethod('POST')) {
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $user contient les valeurs entrées dans le formulaire par le visiteur
+            $form->handleRequest($request);
+
+            // On vérifie que les valeurs entrées sont correctes
+            if ($form->isValid()) {
+
+                //On traite l'ajout d'image
+                /** @var UploadedFile $image */
+                $image = $form['photo']->getData();
+
+                // On vérifie la présence d'un fichier uploadé
+                if ($image) {
+                    $originalFilename = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
+                    // this is needed to safely include the file name as part of the URL
+                    $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $image->guessExtension();
+
+                    //Je vérifie si une image est liée à l'utilisateur
+                    if ($user->getPhoto() !== null) {
+                        //J'appelle le gestionnaire de fichier
+                        $filesystem = new Filesystem();
+                        //Je vérifie si le fichier existe, si oui, symfony le supprime
+                        if ($filesystem->exists($imageURL = $this->getParameter('photos_directory') . "/" . $user->getPhoto())) {
+                            $filesystem->remove([$imageURL]);
+                        }
+                    }
+
+                    //On place le fichier dans le dossier prévu sur le serveur
+                    try {
+                        $image->move(
+                            $this->getParameter('photos_directory'),
+                            $newFilename
+                        );
+                    } catch (FileException $e) {
+                        $this->addFlash('warning', 'L\'envoi de la photo a échoué');
+                    }
+
+                    // On ajoute le nom de l'image à l'utilisateur concerné
+                    $user->setPhoto($newFilename);
+                }
+
+                $this->addFlash('success', 'Bienvenue, inscription terminée');
+
+                // On enregistre notre objet $user dans la base de données, par exemple
+                $this->em->persist($user);
+                $this->em->flush();
+
+                // On redirige vers la page de connexion
+                return $this->redirectToRoute('login', [
+                    'current_menu' => 'login'
+                ]);
+            }
+        }
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+        return $this->render('/pages/form.html.twig', [
+            'form' => $form->createView(),
+            'current_menu' => 'login'
         ]);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,25 +5,68 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('telephone', TextType::class)
-            ->add('adresse', TextType::class)
-            ->add('complement', TextType::class)
-            ->add('codepostal', )
-            ->add('ville')
-            ->add('password')
-            ->add('photo')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('telephone', TelType::class, [
+                'required' => false,
+                'label' => 'Numéro de téléphone'
+            ])
+            ->add('adresse', TextType::class, [
+                'required' => false,
+                'label' => 'Adresse'
+            ])
+            ->add('complement', TextType::class, [
+                'required' => false,
+                'label' => 'Complément d\'adresse'
+            ])
+            ->add('codepostal', IntegerType::class, [
+                'required' => false,
+                'label' => 'Code postal'
+            ])
+            ->add('ville', TextType::class, [
+                'required' => false,
+                'label' => 'Ville'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo (JPG file)',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Merci de n\'envoyer que des jpeg'
+                    ])
+                ],
+            ])
+            ->add('Enregistrer', SubmitType::class)
         ;
     }
 
