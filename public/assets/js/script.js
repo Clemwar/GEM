a6fc8b1e8d7805739261094c30253c06c48c46ac
Clemwar <clemwar@gmail.com>
2020-03-28 18:11:34 +0100

    Integration front bootstrap 1.5/3

---
+++ public/assets/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function(event) {
+
+    /*Scrolling vers ateliers
+    document.getElementById("linka").addEventListener("click", function(){
+        document.getElementById("ateliers").scrollIntoView(false);
+    });
+
+    //Scrolling vers événements
+    document.getElementById("linke").addEventListener("click", function(){
+        document.getElementById("events").scrollIntoView(false);
+    });*/
+
+  });
